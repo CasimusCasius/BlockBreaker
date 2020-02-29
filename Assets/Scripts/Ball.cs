1ab4e3c9c5ceb856fdc1078a54c6716e413e161e
Grzegorz Kazimierczak <grzegorz.kazimierczak.sg@gmail.com>
2020-02-29 14:35:26 +0100

    U5P9 - create LaunchOnMouseClick() , LockBallToPaddle()

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,11 +7,12 @@ public class Ball : MonoBehaviour
 {
     //config params
     [SerializeField] Paddle paddle1;
+    [SerializeField] Vector2 ballVelocityOnStart = new Vector2(2f,10f);
 
     //state
 
     Vector2 paddleToBallVector;
-    
+    bool hasStarted = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,8 +22,26 @@ public class Ball : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (!hasStarted)
+        {
+            LockBallToPaddle();
+            LaunchOnMouseClick();
+            
+        }
+    }
+
+    private void LaunchOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            hasStarted = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(ballVelocityOnStart.x,ballVelocityOnStart.y);
+        }
+    }
+
+    private void LockBallToPaddle()
+    {
         Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
         transform.position = paddlePos + paddleToBallVector;
-
     }
 }

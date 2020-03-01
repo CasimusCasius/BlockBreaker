f2c95ee3d2fed2a32c367c9ebd31b90ff8731322
CasimusCasius <grzegorz.kazimierczak.sg@gmail.com>
2020-03-01 11:28:12 +0100

    U5P17 Add Level Class, add CountBreakableBlocks method
    count breakable block at start of level

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -6,10 +6,16 @@ public class Block : MonoBehaviour
 {
     [SerializeField] AudioClip destroySFX;
 
+    //cashed reference
+    Level level;
+
+    private void Start()
+    {
+        level = FindObjectOfType<Level>();
+        level.CountBreakableBlocks();
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-
-      
         AudioSource.PlayClipAtPoint(destroySFX, Camera.main.transform.position);
         Destroy(gameObject);
     }
